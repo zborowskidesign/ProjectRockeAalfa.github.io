50b670a9b6d4eb6dda6cd5e703396ff2dd844ade
Michal Zborowski <historykus@interia.pl>
2020-06-29 17:36:21 +0200

    collision detect

--- rocket.js
+++ rocket.js
@@ -1,13 +1,11 @@
-const about = document.getElementById('about');
-const main = document.getElementById('main-section');
-const bgcBg = document.querySelector('.bgc-container');
-const cometa = document.querySelector('.cometa');
 const cloudsBgc = document.getElementById('clouds');
 const controls = document.querySelector('.control');
+let flyRocket = document.querySelector('.fly');
+let myHTML = document.querySelector('.myHTML');
 
-//Sekcja latająca rakieta
-const flyRocket = document.querySelector('.fly');
-var up = false,
+
+
+let up = false,
     right = false,
     down = false,
     left = false,
@@ -15,36 +13,37 @@ var up = false,
     y = window.innerHeight/2-130/2
 document.addEventListener('keydown',press)
 function press(e){
-  if (e.keyCode === 38 /* up */ || e.keyCode === 87 /* w */ || e.keyCode === 90 /* z */){
+  if(e.keyCode == 38)
+  if (e.keyCode == 38 /* up */ || e.keyCode == 87 /* w */ || e.keyCode == 90 /* z */){
     up = true
   }
-  if (e.keyCode === 39 /* right */ || e.keyCode === 68 /* d */){
+  if (e.keyCode == 39 /* right */ || e.keyCode == 68 /* d */){
     right = true
   }
-  if (e.keyCode === 40 /* down */ || e.keyCode === 83 /* s */){
+  if (e.keyCode == 40 /* down */ || e.keyCode == 83 /* s */){
     down = true
   }
-  if (e.keyCode === 37 /* left */ || e.keyCode === 65 /* a */ || e.keyCode === 81 /* q */){
+  if (e.keyCode == 37 /* left */ || e.keyCode == 65 /* a */ || e.keyCode == 81 /* q */){
     left = true
   }
 }
 document.addEventListener('keyup',release)
 function release(e){
-  if (e.keyCode === 38 /* up */ || e.keyCode === 87 /* w */ || e.keyCode === 90 /* z */){
+  if (e.keyCode == 38 /* up */ || e.keyCode == 87 /* w */ || e.keyCode == 90 /* z */){
     up = false
   }
-  if (e.keyCode === 39 /* right */ || e.keyCode === 68 /* d */){
+  if (e.keyCode == 39 /* right */ || e.keyCode == 68 /* d */){
     right = false
   }
-  if (e.keyCode === 40 /* down */ || e.keyCode === 83 /* s */){
+  if (e.keyCode == 40 /* down */ || e.keyCode == 83 /* s */){
     down = false
   }
-  if (e.keyCode === 37 /* left */ || e.keyCode === 65 /* a */ || e.keyCode === 81 /* q */){
+  if (e.keyCode == 37 /* left */ || e.keyCode == 65 /* a */ || e.keyCode == 81 /* q */){
     left = false
   }
 }
 function gameLoop(){
-  var div = document.querySelector('.fly')
+ // const div = document.querySelector('.fly')
   if (up){
     y = y - 10
   }
@@ -57,65 +56,58 @@ function gameLoop(){
   if (left){
     x = x - 10
   }
-  div.style.left = x+'px'
-  div.style.top = y+'px'
+  flyRocket.style.left = x + 'px'
+  flyRocket.style.top = y + 'px'
   window.requestAnimationFrame(gameLoop)
 }
 window.requestAnimationFrame(gameLoop)
 
-
-
 window.addEventListener('keypress', function(e){
-  if(e.keyCode === 32){
+  if(e.keyCode == 32){
   about.style.display = 'none';
   bgcBg.style.display = 'none';
   flyRocket.style.display = 'block';
   cloudsBgc.style.display = 'block';
   controls.style.display = 'block'
   document.body.style.backgroundColor = "#0074D9";
+  document.querySelector('.myHTML').style.display = 'block'
+
+  var kwadrat1 = document.querySelector(".myHTML"), //IMO jest szybciej, ale to trza by przetestować
+            span = document.getElementById("kolizja"),
+            wspolrzedneKwadrat1 = kwadrat1.getBoundingClientRect(),
+            wspolrzedneKwadrat2 =  flyRocket.getBoundingClientRect();
+
+  // Wykrywanie kolizji:
+      //musi być odpalone po transition dla poprawnych obliczeń
+      flyRocket.addEventListener('transitionend', function()
+      {
+          //współrzędne trzeba aktualizować przy każdym ruchu
+          wspolrzedneKwadrat1 = kwadrat1.getBoundingClientRect(),
+          wspolrzedneKwadrat2 = flyRocket.getBoundingClientRect();
+          
+          var rect1 = {x: wspolrzedneKwadrat1.left, y: wspolrzedneKwadrat1.top, width: wspolrzedneKwadrat1.width, height: wspolrzedneKwadrat1.height};
+
+          var rect2 = {x: wspolrzedneKwadrat2.left, y: wspolrzedneKwadrat2.top, width: wspolrzedneKwadrat2.width, height: wspolrzedneKwadrat2.height};
+          if (rect1.x < rect2.x + rect2.width &&
+              rect1.x + rect1.width > rect2.x &&
+              rect1.y < rect2.y + rect2.height &&
+              rect1.height + rect1.y > rect2.y) {
+              //span.innerHTML = "Kolizja";
+              document.querySelector('.MyHtmlSkill').style.display = 'block';
+          }
+          else {
+              span.innerHTML = "";
+          };
+      }, false);
+
+
+  
   }
+})
+
+
+
+
  
-})
-// Koniec Sekcja latająca rakieta
+    
 
-
-//Background First | Początek strony
-window.onload = function(){
-	let sky = document.querySelector('#sky');
-			const rocketOne = document.querySelector('#rocket-1');
-		const rocketTwo = document.querySelector('#rocket-2');
-       const rocketThree = document.querySelector('#rocket-3');
-			 const rocketFour = document.querySelector('#cloud-4');
-
-	sky.addEventListener('mousemove',function(e){
-		var pageX = e.clientX - window.innerWidth/2,
-                pageY = e.clientY - window.innerHeight/2;
-  	rocketOne.style.transform = 'translateX(' + (5 + pageX/100) + '%) translateY(' + (5 + pageY/100) + '%)';
-  	rocketTwo.style.transform = 'translateX(-' + (5 + pageX/150) + '%) translateY(-' + (5 + pageY/150) +  '%)';
-  	rocketThree.style.transform = 'translateX(-' + (5 + pageX/200) + '%) translateY(' + (5 + pageY/200) +  '%)';
-  	rocketFour.style.transform = 'translateX(-' + (5 + pageX/25) + '%) translateY(-' + (5 + pageY/25) + '%)';
-	});
-}
-
-function bgc(){
-about.style.display = 'block';
-main.style.display = 'none';
-bgcBg.style.display = 'block';
-
-}
-const span = document.querySelector('span');
-window.addEventListener('keypress', function(e){
-  if(e.keyCode === 48){
-  span.style.animationPlayState = 'running';
-  cometa.style.animationPlayState = 'running';
-  }else{
-    return;
-  }
-  setTimeout('bgc()',1000);
-})
-var card = document.querySelector('.card');
-card.addEventListener( 'click', function() {
-  card.classList.toggle('is-flipped');
-});
-
-//Koniec sekcji Background first
